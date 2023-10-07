c1ccef7781d248341ef6908cf6d4e1786a25439b
Suprelf <mh.suprelf@gmail.com>
2023-10-07 17:16:54 +0300

    minor css changes subscribe to input regex check TODO

--- src/app/components/password-field/password-field.component.ts
+++ src/app/components/password-field/password-field.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
 
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+export class CustomErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(
+    control: FormControl | null): boolean {
+    return !!(control && control.invalid && (control.dirty || control.touched));
   }
 }
 
@@ -16,12 +16,51 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
 })
 export class PasswordFieldComponent implements OnInit {
 
-  constructor() { }
+  passwordFormControl = new FormControl('', [Validators.required]);
+  customMatcher = new CustomErrorStateMatcher();
+  strengthLvl = 'none';
+  
+  easyPasswords = [RegExp(/^[A-Za-z]*$/gm), RegExp(/^[0-9]*$/gm), RegExp(/^[$&+,:;=?@#|\\/'<>.^*()%!_-]*$/gm)]
+  mediumPasswords = [RegExp(/^[\p{P}\p{S}A-Za-z]*$/gm), RegExp(/^[\p{P}\p{S}0-9]*$/gm), RegExp(/^[A-Za-z0-9]*$/gm)]
+  strongPasswords = [RegExp(/^[\p{P}\p{S}A-Za-z0-9]*$/gm)]
+
+
+
+
+
+  constructor() {
+    this.passwordFormControl.valueChanges.subscribe(()=>{
+      const password = this.passwordFormControl.value
+
+      this.strongPasswords.some((regex) => {
+        if ((password?.match(regex)) && (password!='') ) {
+          this.strengthLvl = 'strong'
+          console.log(this.strengthLvl)
+        }
+      })
+
+      this.mediumPasswords.some((regex) => {
+        if ((password?.match(regex)) && (password!='') ) {
+          this.strengthLvl = 'medium'
+          console.log(this.strengthLvl)
+        }
+      })
+      
+      this.easyPasswords.some((regex) => {
+        if ((password?.match(regex)) && (password!='') ) {
+          this.strengthLvl = 'easy'
+          console.log(this.strengthLvl)
+        }
+      })
+    })
+  }
 
   ngOnInit(): void {
   }
 
-  passwordFormControl = new FormControl('', [Validators.required, Validators.email]);
-  matcher = new MyErrorStateMatcher();
+  
+
+  
+
 
 }
